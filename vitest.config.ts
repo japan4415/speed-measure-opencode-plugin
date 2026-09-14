514de7084e4adb84b126f782dd5db5b3b625a265
Yoshihiko Kato <raiden000discord@gmail.com>
2026-09-14 10:11:25 +0900

    Vitestの探索範囲をテストディレクトリに限定

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "node",
+    exclude: [...configDefaults.exclude, "**/.workflow-runs/**"],
     globals: true,
+    include: ["test/**/*.test.ts"],
   },
 });
